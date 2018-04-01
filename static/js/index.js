92bb33c5994aca14927fbd8efe752620ad68ca11
Konrad Wasowicz <exaroth@gmail.com>
2018-04-01 17:39:04 +0100

    Add MUI

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import { initStore } from 'utils/react' 
 import reducersAggregator from 'reducers/store_aggregator';
@@ -9,7 +10,9 @@ import App from 'components/app';
 function initApp() {
   let appContainer = (
     <Provider store={ initStore(reducersAggregator) }>
+      <MuiThemeProvider>
         <App />
+      </MuiThemeProvider>
     </Provider>
   );
   ReactDOM.render(appContainer, document.getElementById('app'));
